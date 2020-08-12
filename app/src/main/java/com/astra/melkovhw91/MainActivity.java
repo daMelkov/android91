0316ba6efeb7d113117f00d8b5ddea9ae1a683d9
Melkov Denis <d.a.melkov@gmail.com>
2020-08-12 10:04:34 +0300

    add styles

--- app/src/main/java/com/astra/melkovhw91/MainActivity.java
+++ app/src/main/java/com/astra/melkovhw91/MainActivity.java
@@ -3,12 +3,35 @@ package com.astra.melkovhw91;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+    private Spinner mLanguagesSpinner;
+    private Spinner mStylesSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
+    }
+
+    private void initViews() {
+        mLanguagesSpinner = findViewById(R.id.spinner_language);
+        mStylesSpinner = findViewById(R.id.spinner_style);
+
+        initSpinners();
+    }
+
+    private void initSpinners() {
+        ArrayAdapter<CharSequence> adapterLanguages = ArrayAdapter.createFromResource(this, R.array.languages, android.R.layout.simple_spinner_item);
+        adapterLanguages.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mLanguagesSpinner.setAdapter(adapterLanguages);
+
+        ArrayAdapter<CharSequence> adapterStyles = ArrayAdapter.createFromResource(this, R.array.styles, android.R.layout.simple_spinner_item);
+        adapterStyles.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mStylesSpinner.setAdapter(adapterStyles);
     }
 }
